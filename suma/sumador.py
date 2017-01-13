7cc984a13022a38db66bcf2369983f7d4578c6cf
Alex <alegrasie@alum.us.es>
2017-01-13 10:09:19 +0100

    Primer commit.

---
+++ suma/sumador.py
@@ -0,0 +1,12 @@
+
+ import suma
+ def ejecucion():
+     print 10
+     assert 10==15
+     assert test()==50
+     print suma(-10,-15)
+ def test():
+    return 20+30
+ 
+ if __name__ == '__main__':
+    ejecucion() 
